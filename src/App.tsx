d60f4480719845c108dc69f8a5514836e28325a0
sankha <sankhasubhradas1@gmail.com>
2025-06-25 20:08:23 +0530

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import Testimonials from './components/Testimonials';
+
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
